0c52abef03d55564588a727252f69b600fc96d90
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2020-02-04 12:12:14 -0300

    Configurando e instalando firebase

--- film-crud/src/main.js
+++ film-crud/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import * as firebase from 'firebase'
+
 import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router';
 
@@ -15,5 +17,17 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAB1Cc3CeL3s8Nb0PSHrbrdX_t7S9FlK0Q",
+      authDomain: "gamecatalog-ec5e8.firebaseapp.com",
+      databaseURL: "https://gamecatalog-ec5e8.firebaseio.com",
+      projectId: "gamecatalog-ec5e8",
+      storageBucket: "gamecatalog-ec5e8.appspot.com",
+      messagingSenderId: "1017991459080",
+      appId: "1:1017991459080:web:647850faf5fedd3a44fcbd",
+      measurementId: "G-QHMGMBYR6W"
+    })
+  }
 }).$mount('#app')
