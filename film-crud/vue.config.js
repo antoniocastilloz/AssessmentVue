8439322ffd72777f4d9f942fc90de8ce78d2b041
antoniocastilloz <47866299+antoniocastilloz@users.noreply.github.com>
2020-02-09 15:52:55 -0300

    Todo para build no github

--- film-crud/vue.config.js
+++ film-crud/vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  // publicPath: '/Vue-GameCatalog/'
+  // Build no Github Obeservação: Descomentar a linha abaixo, salvar, executar o comando de build e depois descartar a alteração.
+  // publicPath: '/Vue-GameCatalog/' 
 }
